4a0ff43dbe3d88a2a186fe802b621a917d4590f8
Elena <fox67rus@mail.ru>
2023-02-11 17:43:30 +0300

    Добавлена команда /about, более читаемый текст вывода результата

--- app.py
+++ app.py
@@ -8,22 +8,31 @@ bot = telebot.TeleBot(TOKEN)
 
 # Обработка команд
 @bot.message_handler(commands=['start', 'help'])
-def start_help(message: telebot.types.Message):
+def command_start_help(message: telebot.types.Message):
     print(message.text)
     text = 'Для начала работы введите команду боту в следующем формате:\n ' \
            '<имя валюты> <в какую валюту переводим> ' \
            '<количество переводимой валюты>\n' \
            'Например, доллар рубль 100\n\n' \
-           'Введите /values, для получения списка доступных валют.'
+           'Введите /values, для получения списка доступных валют.\n' \
+           'Введите /about, для получения информации о боте.\n' \
+           'Введите /help, для получения списка команд.'
     bot.reply_to(message, text)
 
 
 @bot.message_handler(commands=['values'])
-def handle_start_help(message):
+def command_values(message):
     print(message.text)
     text = 'Бот умеет работать со следующими валютами:'
     for key in currency.keys():
         text = '\n'.join((text, key,))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['about'])
+def command_about(message):
+    print(message.text)
+    text = 'Бот написан в качестве контрольного проекта. \n Автор: Павлова Елена (QAP-1019)'
     bot.reply_to(message, text)
 
 
@@ -47,7 +56,7 @@ def convert(message: telebot.types.Message):
         bot.reply_to(message, f'Хьюстон, у нас проблема:\n{e}')
     else:
         text = f'Конвертируем {base} в {quote}: \n ' \
-               f'Цена {amount} {base} в {quote} - {total_base}'
+               f'Цена {amount} {currency[base]}  = {total_base} {currency[quote]}'
         bot.send_message(message.chat.id, text)
 
 
@@ -55,19 +64,20 @@ def convert(message: telebot.types.Message):
 @bot.message_handler(content_types=['photo', 'sticker'])
 def repeat_photo_stick(message: telebot.types.Message):
     print(message.text)
-    bot.reply_to(message, 'Красивая картинка! 😍')
+    bot.reply_to(message, 'Красивая картинка! 😍\n Введите /help для вызова справки.')
 
 
 @bot.message_handler(content_types=['voice', ])
 def repeat_voice(message: telebot.types.Message):
     print(message.text)
-    bot.send_message(message.chat.id, 'У тебя красивый голос, но я ещё не умею распознать голосовые сообщения')
+    bot.send_message(message.chat.id, 'У тебя красивый голос, но я ещё не умею распознать голосовые сообщения\n'
+                                      'Введите /help для вызова справки.')
 
 
 @bot.message_handler(content_types=['document', 'audio'])
 def handle_docs_audio(message):
     print(message.text)
-    bot.reply_to(message, 'Документы я пока не умею открывать')
+    bot.reply_to(message, 'Документы я пока не умею открывать.\n Введите /help для вызова справки.')
 
 
 bot.polling(none_stop=True)
