7af6c18f0f2fe8111c2f926e4ebf3196439b1fb1
Elena <fox67rus@mail.ru>
2023-02-11 13:51:33 +0300

    Создание класса для обработки конвертирования, перенос классов в отдельный файл, настройка зависимостей

--- extensions.py
+++ extensions.py
@@ -1,6 +1,40 @@
+import json
+
+import requests
+
+currency = {
+    'рубль': 'RUB',
+    'доллар': 'USD',
+    'евро': 'EUR'
+}
+
+
 class APIException(Exception):
     pass
 
 
 class CurrencyConverter:
-    pass
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        try:
+            quote_ticker = currency[quote]
+        except KeyError:
+            raise APIException(f'Не удалось найти валюту {quote}.')
+
+        try:
+            base_ticker = currency[base]
+        except KeyError:
+            raise APIException(f'Не удалось найти валюту {base}.')
+
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[currency[base]]
+
+        return total_base
